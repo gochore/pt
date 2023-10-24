81d42289d714530d8234ab14585eec3464755531
Jason Song <i@wolfogre.com>
2023-10-24 17:32:06 +0800

    docs: update comments

--- generic.go
+++ generic.go
@@ -2,12 +2,14 @@
 
 package pt
 
-// P return pointer of v
+// P return pointer of v.
+// It's a short form of "Pointer" or "GetPointer".
 func P[T any](v T) *T {
 	return &v
 }
 
-// V return value of p
+// V return value of p. If p is nil, return zero value of T.
+// It's a short form of "Value" or "GetValue".
 func V[T any](p *T) T {
 	if p == nil {
 		return *new(T)
